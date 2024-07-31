cdfb30f2af7d61b1de04ff63ef39862b656bb696
muhith7 <abdulmuhithnawawi@gmail.com>
2024-07-31 19:41:53 +0700

    Add new color palette for app components

---
+++ lib/constanst/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color green50 = Color(0xFFebfee6);
+  static const Color green100 = Color(0xFFc0fab0);
+  static const Color green200 = Color(0xFFa2f88a);
+  static const Color green300 = Color(0xFF77f554);
+  static const Color green400 = Color(0xFF5df333);
+  static const Color green500 = Color(0xFF34f000);
+  static const Color green600 = Color(0xFF2fda00);
+  static const Color green700 = Color(0xFF25aa00);
+  static const Color green800 = Color(0xFF1d8400);
+  static const Color green900 = Color(0xFF166500);
+// Tambahkan warna lain sesuai kebutuhan
+}
