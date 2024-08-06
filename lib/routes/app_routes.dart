14f391104e5555358a9c5c3531c1795b0b7fdcf4
muhith7 <abdulmuhithnawawi@gmail.com>
2024-08-07 03:58:43 +0700

    feat: Adding Routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,21 @@
+import 'package:capstone/views/about_us/about_us.dart';
+import 'package:capstone/views/get_started/get_started_screen.dart';
+import 'package:capstone/views/home/home_page.dart';
+import 'package:capstone/views/login/login_screen.dart';
+import 'package:capstone/views/register/register_page.dart';
+import 'package:capstone/views/weather/weather_page.dart';
+import 'package:get/get.dart';
+
+import 'app_routes_named.dart';
+
+class AppRoutes {
+  static final routes = [
+    GetPage(
+        name: AppRoutesNamed.pageGetStarted, page: () => GetStartedScreen()),
+    GetPage(name: AppRoutesNamed.pageLogin, page: () => LoginScreen()),
+    GetPage(name: AppRoutesNamed.pageRegister, page: () => RegisterPage()),
+    GetPage(name: AppRoutesNamed.pageHome, page: () => HomePage()),
+    GetPage(name: AppRoutesNamed.pageWeather, page: () => WeatherPage()),
+    GetPage(name: AppRoutesNamed.pageAboutUs, page: () => AboutUs()),
+  ];
+}
---
+++ lib/routes/app_routes_named.dart
@@ -0,0 +1,8 @@
+class AppRoutesNamed {
+  static const pageGetStarted = '/';
+  static const pageLogin = '/page-login';
+  static const pageRegister = '/page-register';
+  static const pageHome = '/page-home';
+  static const pageWeather = '/page-weather';
+  static const pageAboutUs = '/page-about';
+}
